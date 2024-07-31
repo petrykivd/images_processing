5330aca54df4228289b80885173cce52618ecdd2
Dmytro Petrykiv <petrykiv.dmytro19@gmail.com>
2024-07-31 10:22:57 +0300

    Implemented image saver

---
+++ core/image_saver.py
@@ -0,0 +1,32 @@
+import base64
+import io
+import os
+
+import requests
+
+from PIL import Image
+
+from loguru import logger
+
+from config import settings
+
+
+def save_image(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        try:
+            img = Image.open(io.BytesIO(response.content))
+            if img.mode == 'RGBA':
+                img = img.convert('RGB')
+            img.save(os.path.join(settings.DOWNLOAD_FOLDER, filename))
+        except Image.UnidentifiedImageError:
+            logger.warning(f"Cannot identify image file from URL: {url}")
+    else:
+        logger.warning(f"Failed to download image from URL: {url}, status code: {response.status_code}")
+        return
+    return filename
+
+
+def encode_image(image_path):
+    with open(os.path.join(settings.DOWNLOAD_FOLDER, image_path), "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
